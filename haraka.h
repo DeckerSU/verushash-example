573b7f496a3f3b733302c13b0a1bb11daa889d1c
DeckerSU <support@decker.su>
2018-06-26 16:22:26 +0300

    fix? TRUNCSTORE for MSVC build

--- haraka.h
+++ haraka.h
@@ -36,6 +36,7 @@ typedef unsigned long long u64;
 typedef unsigned long u64;
 #endif
 typedef __m128i u128;
+//#define u128 __m128i
 
 extern u128 rc[40];
 
@@ -93,11 +94,20 @@ extern u128 rc[40];
   s2 = _mm_unpackhi_epi32(s1, tmp); \
   s1 = _mm_unpacklo_epi32(s1, tmp);
 
+#ifndef _WIN32
 #define TRUNCSTORE(out, s0, s1, s2, s3) \
   *(u64*)(out) = (u64*)(s0)[1]; \
   *(u64*)(out + 8) = (u64*)(s1)[1]; \
   *(u64*)(out + 16) = (u64*)(s2)[0]; \
   *(u64*)(out + 24) = (u64*)(s3)[0];
+#else
+#define TRUNCSTORE(out, s0, s1, s2, s3) \
+  *(u64*)(out) = *(((u64*)&s0 + 1)); \
+  *(u64*)(out + 8) = *(((u64*)&s1 + 1)); \
+  *(u64*)(out + 16) = *(((u64*)&s2 + 0)); \
+  *(u64*)(out + 24) = *(((u64*)&s3 + 0));
+#endif
+
 
 void load_constants();
 void test_implementations();
